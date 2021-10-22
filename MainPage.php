953ee27257942320311fa88c6fdc07da08892522
codfsh9 <codfsh9@gmail.com>
2021-10-22 11:57:13 -0400

    Create MainPage.php

---
+++ MainPage.php
@@ -0,0 +1,25 @@
+<html>
+	
+	<head>
+	<style type="text/css">
+	.auto-style1 {
+		text-align: center;
+		margin-top: 40px;
+	}
+	</style>
+	</head>
+
+	<body>
+
+	<form action="Dashboard.php" method="post">
+		<div class="auto-style1">
+			
+			<input name="DispDash" type="submit" value="Dashboard"><br><br><br>
+			<input name="AddInv" type="button" value="Add Inventory"><br><br>
+			<br><input name="RemInv" type="button" value="Remove Inventory"><br>
+			<br><br>
+			<input name="ViewSupp" type="button" value="Supplier Information"></div>
+	</form>
+
+	</body>
+</html>
