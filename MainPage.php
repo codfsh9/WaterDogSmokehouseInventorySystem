f57abda768dd45c7125760f0ced0b24e249772b7
RavenJ1305 <ravenj@go.stockton.edu>
2021-11-10 15:21:04 -0500

    Update MainPage.php

--- MainPage.php
+++ MainPage.php
@@ -14,14 +14,20 @@
 	<form action="AddInv.php" method="post">
 		<div class="auto-style1">
 			<input name="AddInv" type="submit" value="Add Inventory"><br><br><br>
-			<input name="ViewSupp" type="button" value="Supplier Information">
 		</div>
 	</form>
 
-	<form action "RemInv.php" method="post">
+	<form action="RemInv.php" method="post">
 		<div class="auto-style1">
 			<input name="RemInv" type="submit" value="Remove Inventory"><br><br><br>
 		</div>
 	</form>
+
+	<form action="ViewSupp.php" method="post">
+		<div class="auto-style1">
+			<input name="ViewSupp" type="submit" value="Supplie Information"><br><br><br>
+		</div>
+	</form>
+
 	</body>
 </html>
