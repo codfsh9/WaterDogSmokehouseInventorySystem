f3b34a5ee5320020dac720684bf5401114639a16
codfsh9 <codfsh9@gmail.com>
2021-11-12 16:15:54 -0500

    Update RemInv.php

--- RemInv.php
+++ RemInv.php
@@ -47,12 +47,61 @@
 </head>
 
 <body style="background-color:#33475b;">
-<h1 style="font-size: xx-large; text-align: center; color:white">Remove Inventory Page</h1>
-	<br>
+<h1 style="font-size: xx-large; text-align: center; color:white;">Add Inventory Page</h1>
+    <br>
     <p>
-    	<button onclick="window.location.href='MainPage.php'">
+        <button onclick="window.location.href='MainPage.php'">
         Back to Homepage
         </button>
-	</p>
+    </p>
+    <!doctype html>
+<html>
+
+<div id="addnewinventory">
+<h2>Add New Inventory Item</h2>
+<form action="" method="post">
+<label>Ingredient ID:</label>
+<input type="text" name="ingredientid" required placeholder="Please Enter Ingredient ID"/><br><br>
+<label>Ingredient Name:</label>
+<input type="text" name="ingredientname" required placeholder="Please Enter Ingredient Name"/><br><br>
+<label>Description:</label>
+<input type="text" name="description" required placeholder="Please Enter Ingredient Description"/><br><br>
+<label>Quantity:</label>
+<input type="number" name="quantity" required placeholder="Please Enter Ingredient Quantity"/><br><br>
+
+<input type="submit" value=" Submit " name="submit"/><br />
+	
+</form>
+	
+</div>
+	
+</html>
+
+<?php
+if(isset($_POST["submit"]))
+{
+include 'dbconfig.php';
+
+$sql = "DELETE FROM ingredients 
+WHERE ing_id=ingredientid";
+
+if ($conn->query($sql) === TRUE) 
+{
+echo "
+    <script type= 'text/javascript'>
+        alert('New record created successfully');
+    </script>";
+} 
+else 
+{
+    echo 
+    "<script type= 'text/javascript'>
+        alert('Error: " . $sql . "<br>" . $conn->error."');
+    </script>";
+}
+
+$conn->close();
+}
+?>
 </body>
 </html>
