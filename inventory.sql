5acb7e751ab703fcfdeb10eb65da445bbc1c4670
codfsh9 <codfsh9@gmail.com>
2021-11-11 21:35:20 -0500

    Update inventory.sql
    Added a basic Ingredients table

--- inventory.sql
+++ inventory.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 07, 2021 at 11:06 PM
+-- Generation Time: Nov 12, 2021 at 03:18 AM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.12
 
@@ -20,6 +20,30 @@ SET time_zone = "+00:00";
 --
 -- Database: `inventory`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ingredients`
+--
+
+CREATE TABLE `ingredients` (
+  `ing_id` int(11) NOT NULL,
+  `ing_name` varchar(150) NOT NULL,
+  `ing_desc` varchar(150) NOT NULL,
+  `ing_q` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Stores the Ingredients for restaurant dishes';
+
+--
+-- Dumping data for table `ingredients`
+--
+
+INSERT INTO `ingredients` (`ing_id`, `ing_name`, `ing_desc`, `ing_q`) VALUES
+(1, 'Plain Bagel', 'N/A', 50),
+(2, '\r\nSesame Bagel', 'N/A', 50),
+(3, 'Poppy Bagel', 'Bagel w/ poppy seeds', 50),
+(4, 'Salt Bagel', 'N/A', 40),
+(5, 'Honey Oat Bagel', 'N/A', 50);
 
 -- --------------------------------------------------------
 
@@ -45,10 +69,26 @@ INSERT INTO `users` (`user_id`, `username`, `password`) VALUES
 --
 
 --
+-- Indexes for table `ingredients`
+--
+ALTER TABLE `ingredients`
+  ADD PRIMARY KEY (`ing_id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `ingredients`
+--
+ALTER TABLE `ingredients`
+  MODIFY `ing_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
