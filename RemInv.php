897535db893e7ea879e65ce90992e7261e8746fe
codfsh9 <codfsh9@gmail.com>
2021-11-12 13:48:55 -0500

    Create RemInv.php

---
+++ RemInv.php
@@ -0,0 +1,11 @@
+<html>
+    <body>
+        <h1 style="text-align: center;">Remove Inventory Page</h1>
+        <br>
+        <p>
+            <button onclick="window.location.href='MainPage.php'">
+            Back to Homepage
+            </button>
+        </p>
+    </body>
+</html>
