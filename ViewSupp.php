133dcd920dc428dbec75e6f566648842ebb54497
codfsh9 <codfsh9@gmail.com>
2021-11-12 13:48:52 -0500

    Create ViewSupp.php

---
+++ ViewSupp.php
@@ -0,0 +1,11 @@
+<html>
+    <body>
+    <h1 style="text-align: center;">Supplier Info Page</h1>
+    <br>
+    <p>
+        <button onclick="window.location.href='MainPage.php'">
+        Back to Homepage
+        </button>
+    </p>       
+    </body>
+</html>
