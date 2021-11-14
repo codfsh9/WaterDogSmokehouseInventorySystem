d91bb01083a69804615bae614a21bb149d524783
codfsh9 <codfsh9@gmail.com>
2021-11-14 17:14:38 -0500

    SQL changes

--- inventory.sql
+++ inventory.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.15.9
--- https://www.phpmyadmin.net
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Nov 12, 2021 at 08:13 PM
--- Server version: 5.6.37
--- PHP Version: 5.6.31
+-- Host: 127.0.0.1
+-- Generation Time: Nov 14, 2021 at 11:14 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -26,13 +27,13 @@ SET time_zone = "+00:00";
 -- Table structure for table `ingredients`
 --
 
-CREATE TABLE IF NOT EXISTS `ingredients` (
+CREATE TABLE `ingredients` (
   `ing_id` int(11) NOT NULL,
-  `ing_name` varchar(150) NOT NULL,
-  `ing_desc` varchar(150) NOT NULL,
-  `ing_q` int(255) NOT NULL,
-  `ing_q_max` int(255) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COMMENT='Stores the Ingredients for restaurant dishes';
+  `ing_name` varchar(50) NOT NULL,
+  `ing_desc` varchar(100) NOT NULL,
+  `ing_q` int(25) NOT NULL,
+  `ing_q_max` int(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Stores the Ingredients for restaurant dishes';
 
 --
 -- Dumping data for table `ingredients`
@@ -43,7 +44,14 @@ INSERT INTO `ingredients` (`ing_id`, `ing_name`, `ing_desc`, `ing_q`, `ing_q_max
 (2, '\r\nSesame Bagel', 'N/A', 50, 50),
 (3, 'Poppy Bagel', 'Bagel w/ poppy seeds', 50, 50),
 (4, 'Salt Bagel', 'N/A', 40, 50),
-(5, 'Honey Oat Bagel', 'N/A', 50, 50);
+(5, 'Honey Oat Bagel', 'N/A', 50, 50),
+(6, 'Eggs', 'N/A', 216, 300),
+(7, 'Bacon', 'N/A', 100, 300),
+(8, 'Cheese', 'Cheddar', 100, 150),
+(9, 'Lettuce', 'Romaine', 60, 100),
+(10, 'Tomatoes', 'Plum type', 67, 100),
+(11, 'Raw Onions', 'N/A', 50, 50),
+(12, 'Beef', 'Angus', 100, 150);
 
 -- --------------------------------------------------------
 
@@ -51,7 +59,7 @@ INSERT INTO `ingredients` (`ing_id`, `ing_name`, `ing_desc`, `ing_q`, `ing_q_max
 -- Table structure for table `users`
 --
 
-CREATE TABLE IF NOT EXISTS `users` (
+CREATE TABLE `users` (
   `user_id` int(10) NOT NULL,
   `username` varchar(30) NOT NULL,
   `password` varchar(50) NOT NULL
@@ -72,23 +80,15 @@ INSERT INTO `users` (`user_id`, `username`, `password`) VALUES
 -- Indexes for table `ingredients`
 --
 ALTER TABLE `ingredients`
-  ADD PRIMARY KEY (`ing_id`);
+  ADD UNIQUE KEY `ing_id` (`ing_id`);
 
 --
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`user_id`);
+COMMIT;
 
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `ingredients`
---
-ALTER TABLE `ingredients`
-  MODIFY `ing_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
